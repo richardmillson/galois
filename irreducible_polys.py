533e08bc0026cb59ac8f54e0143175ed7699927d
richardmillson <8217613+richardmillson@users.noreply.github.com>
2016-06-28 15:24:56 -0400

    Polynomial coefficients in order of increasing degree now

--- irreducible_polys.py
+++ irreducible_polys.py
@@ -8,7 +8,7 @@ import itertools
 
 def has_roots(poly, p):
     """
-    evaluates the polynomial at all possible residues,
+    evaluates the polynomial poly at all possible residues of F_p
     returns true if the polynomial has a root
     :param poly: numpy.polynomial.polynomial
     :param p: int
@@ -19,7 +19,8 @@ def has_roots(poly, p):
 
 def irred_polys(p, d):
     """
-    returns a tuple of polynomials that do not have roots
+    returns a tuple of polynomials of degree d
+    that do not have any roots over the field of p elements
     :param p: int
     :param d: int
     :return: tuple(numpy.polynomial.polynomial)
@@ -29,13 +30,13 @@ def irred_polys(p, d):
 
 def cand_polys(p, d):
     """
-    returns a generator for all polynomials in the given field
-    stored as a tuple of coefficients from highest to lowest power
-    note that polynomials with zero constant term are excluded
+    returns all candidate polynomials of degree d over the field of p elements
+    as a generator of tuples of polynomial coefficients in order of increasing degree
+    polynomials with a zero constant term are excluded
     because they are guaranteed to have a root at x = 0
     :param p: int
     :param d: int
     :return: generator of numpy.polynomial.polynomial
     """
-    cand_polys = itertools.product(range(p), repeat=d)
-    return ((1,) + poly for poly in cand_polys if not poly[-1] == 0)
+    polys = itertools.product(range(p), repeat=d)
+    return (poly + (1,) for poly in polys if not poly[0] == 0)
